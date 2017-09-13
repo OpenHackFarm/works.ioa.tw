861a674c1469c93db5353b758eeb94214a7836e3
yan <a0726h77@gmail.com>
2017-09-13 17:41:07 +0800

    Merge Towns

---
+++ merge_city_town.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import json
+import requests
+import io
+
+towns = []
+
+c = requests.get('https://works.ioa.tw/weather/api/all.json')
+
+for city in json.loads(c.text):
+    print 'fetch', city['id']
+    t = requests.get('https://works.ioa.tw/weather/api/cates/%d.json' % int(city['id']))
+    towns = towns + json.loads(t.text)['towns']
+
+with io.open('towns.json', 'w', encoding='utf8') as json_file:
+    data = json.dumps(towns, sort_keys=False, indent=4, separators=(',', ': '), ensure_ascii=False)
+    json_file.write(data)
